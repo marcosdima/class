3bb6f108874d7d8b2a20531f440d86a17cf804ab
marcosdima <marcosdimatteo.wgr@gmail.com>
2024-03-07 17:41:21 -0300

    Class: useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [aguante, aguanteQue] = useState("...");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Aguante{aguante} </h1>
+      <button onClick={ () => aguanteQue(" BOKITA") } > Que? </button>
     </div>
   );
 }
